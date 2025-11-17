17c44fc5614431d74c83008dfb893720f30dcf11
ebdk <ebdk@users.noreply.github.com>
2025-11-17 06:34:51 +0000

    Scaffolding from Spring Initializr

---
+++ src/test/java/com/example/transaction_core/TransactionCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transaction_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
